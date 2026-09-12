277c09260103ecf9286ab545be189b655e2b8a9d
Gabriel Moterani <contatomoterani@gmail.com>
2026-09-12 03:52:25 +0000

    test(cofre): aviso de texto puro e porta para a tela de addons

--- test/menu_grid_test.dart
+++ test/menu_grid_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:roms_downloader/widgets/menu_grid/menu_grid.dart';
+import 'package:roms_downloader/screens/addons_screen.dart';
+import 'package:roms_downloader/screens/menu_screen.dart';
 
 void main() {
   testWidgets('renders a tile per spec and fires onTap of the tapped tile', (tester) async {
@@ -27,5 +29,16 @@ void main() {
     await tester.pump();
 
     expect(tapped, ['servers']);
+  });
+
+  testWidgets('as tiles de Tools levam para os Addons', (tester) async {
+    final abertas = <Type>[];
+    final tiles = toolsTiles((tela) => abertas.add(tela.runtimeType));
+
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: MenuGrid(tiles: tiles))));
+    await tester.tap(find.text('Addons'));
+    await tester.pump();
+
+    expect(abertas, [AddonsScreen]);
   });
 }
